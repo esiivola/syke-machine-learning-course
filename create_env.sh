afcfccfd83b35c51ba1c828b06119246cecc1dfd
Eero Siivola <eero.siivola@aalto.fi>
2021-03-01 15:56:31 +0200

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -6,5 +6,8 @@ cd ~
 git clone https://github.com/esiivola/syke-machine-learning-course
 
 cd ./syke-machine-learning-course
+apt-get install libproj-dev proj-data proj-bin  
+apt-get install libgeos-dev  
+
 pip install -r requirements.txt
 #conda install -c conda-forge cartopy=0.18.0
