06db6704f737679964047bbf65b69941b7b7d7f5
Siivola Eero <Eero.Siivola@env.fi>
2021-03-01 11:27:15 +0200

    trying to make csc notebooks work

---
+++ create_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+cd /home
+
+git clone https://github.com/esiivola/syke-machine-learning-course
+
+cd /home/syke-machine-learning-course
+
+conda env create -f environment.yml
+
+conda activate SYKE-ML-ENV
