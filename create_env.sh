28d259b94681a0359e112793c05328ce57091305
Eero Siivola <eero.siivola@aalto.fi>
2021-03-03 09:22:49 +0200

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,6 @@
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-cd /data
-
 git clone https://github.com/esiivola/syke-machine-learning-course
 
-cd /data/syke-machine-learning-course
+cd syke-machine-learning-course
