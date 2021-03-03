75bc26570bff5c9c1229de5529af099171e7b40a
Eero Siivola <eero.siivola@aalto.fi>
2021-03-03 08:56:12 +0200

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,13 +1,8 @@
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-cd ~
+cd /data
 
 git clone https://github.com/esiivola/syke-machine-learning-course
 
-cd ./syke-machine-learning-course
-apt-get install libproj-dev proj-data proj-bin  
-apt-get install libgeos-dev  
-
-pip install -r requirements.txt
-#conda install -c conda-forge cartopy=0.18.0
+cd /data/syke-machine-learning-course
